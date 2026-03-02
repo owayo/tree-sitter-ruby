a506eb3cc283ad99728f40f0a1d87df33d1c0af4
owayo <yoheiowa@gmail.com>
2026-03-03 00:26:41 +0900

    fix: 行継続パターンの検証と解析整合性保証

--- bindings/node/binding_test.js
+++ bindings/node/binding_test.js
@@ -4,6 +4,6 @@ const { test } = require("node:test");
 const Parser = require("tree-sitter");
 
 test("can load grammar", () => {
-  const parser = new Parser();
-  assert.doesNotThrow(() => parser.setLanguage(require(".")));
+	const parser = new Parser();
+	assert.doesNotThrow(() => parser.setLanguage(require(".")));
 });
--- bindings/node/index.d.ts
+++ bindings/node/index.d.ts
@@ -1,27 +1,27 @@
 type BaseNode = {
-  type: string;
-  named: boolean;
+	type: string;
+	named: boolean;
 };
 
 type ChildNode = {
-  multiple: boolean;
-  required: boolean;
-  types: BaseNode[];
+	multiple: boolean;
+	required: boolean;
+	types: BaseNode[];
 };
 
 type NodeInfo =
-  | (BaseNode & {
-      subtypes: BaseNode[];
-    })
-  | (BaseNode & {
-      fields: { [name: string]: ChildNode };
-      children: ChildNode[];
-    });
+	| (BaseNode & {
+			subtypes: BaseNode[];
+	  })
+	| (BaseNode & {
+			fields: { [name: string]: ChildNode };
+			children: ChildNode[];
+	  });
 
 type Language = {
-  name: string;
-  language: unknown;
-  nodeTypeInfo: NodeInfo[];
+	name: string;
+	language: unknown;
+	nodeTypeInfo: NodeInfo[];
 };
 
 declare const language: Language;
